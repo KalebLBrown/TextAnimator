20c9d3931cc1798136fba83fa559fbbabbce2385
KalebLBrown <kaleb.brown@digipen.edu>
2025-01-17 15:51:16 -0800

    action list updates correctly now

--- textanimator/addons/textanimator/animatedtext.gd
+++ textanimator/addons/textanimator/animatedtext.gd
@@ -5,6 +5,9 @@ var actionlist: ActionList = ActionList.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Since the actionlist is a node, we have to add it as a child at runtime
+	add_child(actionlist)
+	
 	var action: TestAction = TestAction.new(3.0, 1.0)
 	actionlist.AddAction(action)
 	pass # Replace with function body.
@@ -12,5 +15,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	actionlist._process(delta)
 	pass
